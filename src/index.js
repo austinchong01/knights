2be877533b20bd259e5c91e07569f985bd016835
Austin <austin.inhyuk.chong@gmail.com>
2025-04-07 18:51:06 -0700

    Clean Up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { coordToSpace, spaceToCoord, createAdjacencyList } from "./graph.js";
 
-function BFS(start, end){
+function BFS(start, end) {
   const queue = [start];
-  
+
   const visited = new Array(64).fill(false);
   visited[start] = true;
 
@@ -12,7 +12,7 @@ function BFS(start, end){
     if (curr === end) break;
     // queue neighbors
     for (let i = 0; i < adjList[curr].length; i += 1)
-      if (!visited[adjList[curr][i]]){
+      if (!visited[adjList[curr][i]]) {
         queue.push(adjList[curr][i]);
         visited[adjList[curr][i]] = true;
         prev[adjList[curr][i]] = curr;
@@ -21,30 +21,23 @@ function BFS(start, end){
   return prev;
 }
 
-function createPath(start, end, prev){
+function createPath(end, prev) {
   let path = [];
-  for (let i = end; i != null; i = prev[i]){
-    path.push(i)
-  }
-  path = path.reverse();
-  return path;
+  for (let i = end; i != null; i = prev[i]) path.push(i);
+  return path.reverse();
 }
 
 function knightMoves(start, end) {
-  const prev = BFS(start, end)
-  return createPath(start, end, prev)
+  const prev = BFS(start, end);
+  return createPath(end, prev);
 }
 
 const adjList = createAdjacencyList();
 
 const start = coordToSpace([0, 0]);
-const end = coordToSpace([3, 3]);
+const end = coordToSpace([7, 7]);
 const path = knightMoves(start, end);
-for (let i = 0; i < path.length; i += 1){
-  path[i] = spaceToCoord(path[i]);
-}
+for (let i = 0; i < path.length; i += 1) path[i] = spaceToCoord(path[i]);
 
 console.log(`You made it in ${path.length - 1} moves! Here is your path:`);
-for (let i = 0; i < path.length; i += 1){
-  console.log(path[i])
-}
+for (let i = 0; i < path.length; i += 1) console.log(path[i]);
